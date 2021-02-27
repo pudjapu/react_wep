2a790b5a5c56934664213e00ba36310f62d13e76
pudjapu <tanakorn4567@gmail.com>
2021-02-27 19:15:33 +0700

    edid layout

--- hod_web/src/Componet/layout.js
+++ hod_web/src/Componet/layout.js
@@ -30,7 +30,7 @@ class compro_1_leyout extends React.Component {
         <Layout>
           <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
             <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} style={{minHeight:'100vh'}}>
               <Menu.Item key="1" icon={<UserOutlined />}>
                 nav 1
               </Menu.Item>
