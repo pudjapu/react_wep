8a218274f03a2d201b9683cdd8f06c74f89d3719
pudjapu <tanakorn4567@gmail.com>
2021-02-28 18:08:52 +0700

    add page

---
+++ hod_web/src/Componet/det.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class det extends React.Component{
+    render(){
+        return(
+            <div>DET</div>
+        );
+    }
+}
+export default det
--- hod_web/src/Componet/layout.js
+++ hod_web/src/Componet/layout.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../App.css';
 
+import Home from './home';
+import Det from './det';
+
 import { BookTwoTone,HomeTwoTone,AppstoreTwoTone  } from '@ant-design/icons';
 
 import { Layout, Menu } from 'antd';
@@ -11,14 +14,27 @@ class compro_1_leyout extends React.Component {
     
     state = {
         collapsed: false,
-        at_cack: 1
+        at_cack: <Home/>
       };
     
       toggle = () => {
         this.setState({
           collapsed: !this.state.collapsed,
+          
         });
       };
+
+    headleClick= (e) => {
+      
+      this.setState({
+        at_cack: this.state.at_cack = <Det/>})
+    }
+
+    headleClick_HOME= (e) => {
+      
+      this.setState({
+        at_cack: this.state.at_cack = <Home/>})
+    }
 
     render(){
       return(
@@ -27,29 +43,29 @@ class compro_1_leyout extends React.Component {
           <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
             <div className="logo" />
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} style={{minHeight:'100vh',marginTop: 60,fontSize: '15px'}}>
-              <Menu.Item key="1" icon={<HomeTwoTone />}>
+              <Menu.Item key="1" icon={<HomeTwoTone />}onClick={this.headleClick_HOME}>
                 Home
               </Menu.Item>
-              <Menu.Item key="2" icon={<BookTwoTone />}>
+              <Menu.Item key="2" icon={<BookTwoTone />}onClick={this.headleClick}>
                 nav 2
               </Menu.Item>
-              <Menu.Item key="3" icon={<BookTwoTone />}>
+              <Menu.Item key="3" icon={<BookTwoTone />}onClick={this.headleClick}>
                 nav 3
               </Menu.Item>
-              <Menu.Item key="4" icon={<BookTwoTone />}>
+              <Menu.Item key="4" icon={<BookTwoTone />}onClick={this.headleClick}>
                 nav 4
               </Menu.Item>
-              <Menu.Item key="5" icon={<BookTwoTone />}>
+              <Menu.Item key="5" icon={<BookTwoTone />}onClick={this.headleClick}>
                 nav 5
               </Menu.Item>
-              <Menu.Item key="6" icon={<BookTwoTone />}>
+              <Menu.Item key="6" icon={<BookTwoTone />}onClick={this.headleClick}>
                 nav 6
               </Menu.Item>
             </Menu>
           </Sider>
           <Layout className="site-layout">
             <Header className="site-layout-background" >
-              <div style={{ textAlign: 'left' ,paddingLeft: 0,marginLeft: -50 ,fontSize: '20px'}}>
+              <div style={{ textAlign: 'left' ,paddingLeft: 0,marginLeft: -47 ,fontSize: '20px'}}>
               {React.createElement(this.state.collapsed ? AppstoreTwoTone  : AppstoreTwoTone , {
                 className: 'trigger',
                 onClick: this.toggle,
