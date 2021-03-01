ec9926710d86f05ac9c30ceaffc41c0c758b9881
pudjapu <tanakorn4567@gmail.com>
2021-03-02 00:58:32 +0700

    เพิ่มสี

--- hod_web/src/Componet/bisection_method.js
+++ hod_web/src/Componet/bisection_method.js
@@ -7,29 +7,64 @@ import '../css/bisecton.css';
 class Bisection extends React.Component{
 
     state = {
-        A: '0'
+        Equation: '0',
+        XL: '1.0',
+        XR: '2.0',
+        ERROR: '0.0001',
       };
 
 
-    getvalu = (e) => {
+    getEquation = (e) => {
         this.setState({
-            A: e.target.value,
+            Equation: e.target.value,
+        });
+    };
+
+    getXL = (e) => {
+        this.setState({
+            XL: e.target.value,
+        });
+    };
+
+    getXR = (e) => {
+        this.setState({
+            XR: e.target.value,
+        });
+    };
+
+    getERR= (e) => {
+        this.setState({
+            ERROR: e.target.value,
         });
     };
 
     show_value = (e) =>{
-        console.log(this.state.A)
+        console.log(this.state.Equation)
+
+        let Equation = this.state.Equation
+        Equation = eval(Equation)
+
+        console.log(Equation)
     }
 
     render(){
         return(
-            <div className="allinbisetion">
-            <h2>Bisection Method</h2>
-            <div>
-            <span><Input placeholder="x^3+4" onChange={this.getvalu} className="Input"/></span>
-            <Button type="primary" onClick={this.show_value}>Calculate</Button>
-            </div>
-            <h1></h1>
+            <div className="allinbisetion" >
+                <div className="in_box">
+                    <h2>Bisection Method</h2>
+                    <div>
+                        <span><Input placeholder="x^3+4" onChange={this.getEquation} className="Input"/></span>
+                        <span className="Calculate_Button"><Button type="primary" onClick={this.show_value} >Calculate</Button></span>
+                    </div>
+                    <div>
+                        <span className="Text_Input_2"> XL : </span>
+                        <span><Input placeholder="1.0" onChange={this.getvalu} className="Input_2"/></span>
+                        <span className="Text_Input_2"> XR : </span>
+                        <span><Input placeholder="2.0" onChange={this.getvalu} className="Input_2"/></span>
+                        <span className="Text_Input_2"> ERROR : </span>
+                        <span><Input placeholder="0.0001" onChange={this.getvalu} className="Input_2"/></span>
+                    </div>
+                </div>
             </div>
 
         );
