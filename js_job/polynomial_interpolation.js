00eb460e0f514b6a28edf29fdb1c8f5224b79fc0
pudjapu <tanakorn4567@gmail.com>
2021-03-16 19:50:58 +0700

    ลบคอมเม้น

--- js_job/polynomial_interpolation.js
+++ js_job/polynomial_interpolation.js
@@ -7,4 +7,8 @@ var interpolatingPolynomial = require('interpolating-polynomial'),
                                  [60000, 9.6879],
                                  [80000, 9.5682]]);
 
+
+    // f = interpolatingPolynomial([[0, 9.81],
+    //                              [80000, 9.5682]]);
+
 console.log(f(42000))
