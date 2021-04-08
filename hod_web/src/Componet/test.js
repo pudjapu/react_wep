3d871be353ae2019a07a04810f5c357acda75a7a
pudjapu <tanakorn4567@gmail.com>
2021-04-08 23:57:04 +0700

    test api

--- hod_web/src/Componet/test.js
+++ hod_web/src/Componet/test.js
@@ -1,9 +1,29 @@
 import React from 'react';
+const axios = require('axios');
+
+
+let apiUrl = "http://localhost:4200/member/"
+let Name = document.querySelector('name')
 
 class test extends React.Component{
+
+    showData = (e) => {
+        axios.get(apiUrl)
+        .then(function (response) {
+            response.
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log("error");
+          })
+          
+    };
+
     render(){
         return(
-            <div>test</div>
+            <div>
+                <button onClick={this.showData}>XD</button>
+            </div>
         )
         
     }
