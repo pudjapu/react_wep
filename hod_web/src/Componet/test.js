d6d427d583a695a17978c8de12c7cf1b44dc3c00
pudjapu <tanakorn4567@gmail.com>
2021-04-10 06:02:42 +0700

    ทำ api

--- hod_web/src/Componet/test.js
+++ hod_web/src/Componet/test.js
@@ -2,21 +2,20 @@ import React from 'react';
 const axios = require('axios');
 
 
-let apiUrl = "http://localhost:4200/member/"
-let Name = document.querySelector('name')
+let apiUrl = "http://localhost:4200/member/1"
+let results
 
 class test extends React.Component{
-
+    
+    state = {
+        item: [],
+    }
+    
     showData = (e) => {
-        axios.get(apiUrl)
-        .then(function (response) {
-            
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log("error");
-          })
-          
+        fetch(apiUrl)
+          .then(results => results.json())
+          .then(results => this.setState({item: results}))
+        console.log(results)
     };
 
     render(){
