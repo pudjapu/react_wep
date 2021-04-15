ecfa1b20f50e932b1b8852d74040b54c069d7860
pudjapu <tanakorn4567@gmail.com>
2021-04-15 09:33:25 +0700

    add new 2 input

--- hod_web/src/Componet/Source/Matrix.js
+++ hod_web/src/Componet/Source/Matrix.js
@@ -24,4 +24,26 @@ class Matrix extends React.Component{
     }
 }
 
-export {Matrix}
+class Matrix2Input extends React.Component{
+
+    createMatrix(){
+        let arrMatrix = []
+        for(let i=0;i<this.props.row;i++){
+            for(let j=0;j<2;j++){
+                arrMatrix.push(<Input className='matrix' name={i.toString()+','+j.toString()} onChange={this.props.onChange} value={this.props.value[i][j]}/>)
+            }
+            arrMatrix.push(<div></div>)
+        }
+        return arrMatrix
+    }
+
+    render(){
+        return(
+            <div>
+                {this.createMatrix()}
+            </div>
+        )
+    }
+}
+
+export {Matrix,Matrix2Input}
